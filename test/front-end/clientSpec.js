d3062e4f2bd3f7ab8b477eb39f39672ca0978fbf
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-01 18:37:37 +0300

    Renaming files to serve istanbul coverage test

--- test/front-end/clientSpec.js
+++ test/front-end/clientSpec.js
@@ -1,6 +1,6 @@
-var test = require('../../client/app.js');
+var path = require('path');
 var expect = require ('chai').expect;
-
+var test = require(path.join(__dirname,'../../' ,'./client/app.js'));
 describe("something else", function (){
 	it("this is front-end", function (){
 		expect(true).to.be.true;
