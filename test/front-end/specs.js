74bced8a44f4325de5e4b7df431eca0a2780b9e4
Mihyar <eng.mihyear@gmail.com>
2016-08-01 17:10:19 +0300

    adding test files

---
+++ test/front-end/specs.js
@@ -0,0 +1,8 @@
+var test = require('../../client/app.js');
+var expect = require ('chai').expect;
+
+describe("something else", function (){
+	it("this is front-end", function (){
+		expect(true).to.be.true;
+	})
+})
---
+++ test/server/specs.js
@@ -0,0 +1,8 @@
+var test = require('../../client/app.js');
+var expect = require ('chai').expect;
+
+describe("anything", function (){
+	it("should work", function (){
+		expect(true).to.be.false
+	})
+})
