768cca7ec3c743ea23df0c7181a1b0fad9f0058f
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-02 19:45:03 +0300

    SetUp signin method

--- server/User/userController.js
+++ server/User/userController.js
@@ -43,12 +43,26 @@ module.exports={
 	signin : function(req, res){
 		var username = req.body.username;
 		var password = req.body.password;
+		User.findOne({username: username})
+      		.exec(function (error, user) {
+       			if (!user) {
+     		 	    res.status(500).send(new Error('User does not exist'));
+    		    } else {
+       			    User.comparePassword(password,user.password, res, function(found){
+        		        if(!found){
+       				       res.status(500).send('Wrong Password');
+      			        } else {
+     			            var token = jwt.encode(user, 'secret');
+         			        res.setHeader('x-access-token',token);
+                            res.json({token: token});
+                        }
+                    });
+                }
+            });
 	},
 
 	signup : function(req, res){
 		var username = req.body.username;
-		var password = req.body.password;
-
 			User.findOne({username: username})
 	    		.exec(function(error,user){
 			        if(user){
