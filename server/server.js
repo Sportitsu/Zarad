3ff716f9d294071f95e8e548deff1dbea9d7e348
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-02 10:52:08 +0300

    Setup server to listen variable for address function

--- server/server.js
+++ server/server.js
@@ -8,14 +8,15 @@ var app = express();
 var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/zarad';
 var port = process.env.PORT || 8000;
 
-mongoose.connect(mongoURI);
-
+mongoose.createConnection(mongoURI);
 
 // configure server with all routing plus middleware
 require('./config/middleware.js')(app,express);
 require('./config/routes.js')(app, express);
 
 // start listening on port 8000
-app.listen(port , function(){
-	console.log('List')
+var listener = app.listen(port , function(){
+	console.log('Listening on port ' + listener.address().port); // Listening port
 })
+
+module.exports = app;
