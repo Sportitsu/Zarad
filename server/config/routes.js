08b088d75836774fd88b3a4d7556b368907f3b58
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-02 14:10:00 +0300

    Setting up routes with orders to serve client side

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,20 +1,31 @@
 // Require all the controller functions here 
 var helpers = require('./helpers.js'); // our custom middleware
 // Waiting for database setup 
-
 module.exports = function(app,express){
-
+	// Home page Routes
 	app.get('/api/home' , function(req,res){
 		res.status(200).send('Connected to home');
 	});
+	// Just for testing mocha
 
-
+	// Admin Page Routes
 	app.post('/api/admin' , function(req,res){
+		// console.log(req.body);
+		console.log(req.params.username)
+		// TO DO ....
+	});
+
+	app.get('/api/admin/:username' , function(req,res){
 		console.log(req.body);
-		res.status(201).send('You are not convincing me to give you aanything')
+
 	})
 
 
+	// Club Page Routes
+
+
+
+	// User Page Routes
 
     // If a request is sent somewhere other than the routes above,
     // send it through our custom error handler
