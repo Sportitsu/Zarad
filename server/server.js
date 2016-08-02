3c140ce76f2964f3bd846635f463440d95213bcd
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-02 18:11:04 +0300

    Added new database so that when we run npm test it does not interfere with our local database

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 var mongoose = require('mongoose');
 var express = require('express');
-
+var config = require('./_config');
 
 var app = express();
 
 // connect to mongoDB database
-var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/zarad';
+// var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/zarad';
 var port = process.env.PORT || 8000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongoURI);
+mongoose.connect(config.mongoURI[app.settings.env],function(err, res){
+	if(err){
+		console.log('Error Connecting to the database. ' + err);
+	} else {
+		console.log('Connected to Database ' + config.mongoURI[app.settings.env])
+	}
+});
 
 // configure server with all routing plus middleware
 require('./config/middleware.js')(app,express);
