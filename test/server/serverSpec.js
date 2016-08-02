ce19a72ca44eb375feb47de79e87cf9d8307eba9
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-02 22:39:21 +0300

    Updating latest User database with tests (#13)
    * Resolving conflicts
    
    * Handling error for tests
    
    * Fixing require key
    
    * Updated tests to handle errors
    
    * Solved mongoose deprecated promise issue
    
    * Adding preparation tests for rest of Databases
    
    * Excluded userModel file from coverage
    
    * Updated typos to work with tests
    
    * Fixed typo function mistake and added route for getting all users
    
    * Updated Spec to work for MVP Functions
    
    * Excluded server.js from test coverage
    
    * Added new database so that when we run npm test it does not interfere with our local database
    
    * set the environment node_env path to test
    
    * Added new file that holds the configuration of our new updating of mongo DB
    
    * Added new country key
    
    * Added the method signup
    
    * Added new required keys for the user
    
    * added signup routes for user to signup
    
    * Done writing tests for user signup
    
    * SetUp signin method
    
    * Added The compare Password function
    
    * Added unit and integration test for signin method in userController
    
    * Added checkAuth method
    
    * Updated the config routes .js to handle CheckAuth method
    
    * Added the server Spec test for check Auth but not done yet

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -1,3 +1,4 @@
+process.env.NODE_ENV = 'test';
 var should = require('chai').should();
 var expect = require ('chai').expect;
 var path = require('path')
@@ -6,17 +7,15 @@ var server = require(path.join(__dirname,'../../' ,'./server/server.js'));
 var chai = require('chai')
       ,chaiHttp = require('chai-http');
 
+
 chai.use(chaiHttp);
 var Admin = require('../../server/Admin/adminModel');
+var User = require('../../server/User/userModel');
+var userController = require('../../server/User/userController');
 var request = supertest.agent(server);
 
-
-beforeEach(function(){
-	// TODO FIND FAKE POSTS AND DELETE THEM FROM DATABASE. 
-})
-
-describe("Server", function (){
-	xdescribe('/GET' , function(done){
+describe("Integration Server Database test", function (){
+	describe('/GET' , function(done){
 		it("is just for testing mocha and chai ", function (done){
 			chai.request(server)
 				.get('/api/home')
@@ -55,7 +54,7 @@ describe("Server", function (){
 			    'lastName' : 'Man',
 			    'email' : 'ironman@avengers.com'
 			});
-			testAdmin.save(function(err,data){	
+			testAdmin.save(function(error,data){	
 				 request.get('/api/admin/'+ data.username)
 						.set('Accept','application/json')
 						.expect(200)
@@ -73,6 +72,24 @@ describe("Server", function (){
 							done();
 						});
 			})
+		});
+
+
+		it('should fail when sending wrong username' , function(done){
+			var testAdmin = new Admin({
+				'username' : 'super',
+			    'password' : '123', 
+			    'firstName' : 'Iron' ,
+			    'lastName' : 'Man',
+			    'email' : 'ironman@avengers.com'
+			});
+			request.get('/api/admin/dont' )
+				   .set('Accept' , 'application/json')
+				   .expect(500)
+				   .end(function(err,res){
+				   		expect(res.status).to.be.equal(500);
+				   		done();
+				   })
 		})
 
 		it('should add admin with response 201 OK' , function(done){
@@ -96,6 +113,247 @@ describe("Server", function (){
 				});
 		});
 
+		it('should fail when missing username key' , function(done){
+			chai.request(server)
+				.post('/api/admincreate')
+				.send({
+					'password' : 'test' ,
+					'email' : 'blaBla'
+				})
+				.end(function(err,res){
+					expect(res.status).to.be.equal(500);
+					done();
+				})
+		})
 
-	})
+		it('should fail when creating an existing admin', function(done){
+			chai.request(server)
+				.post('/api/admincreate')
+				.send({
+					'username' : 'admin-memf',
+				    'password' : '123', 
+				    'firstName' : 'testing' ,
+				    'lastName' : 'noneOfYourBusiness',
+				    'email' : 'notGonnaTellYou'
+				})
+				.end(function(err ,res){
+					expect(res.status).to.be.equal(500);
+					done();
+				})
+		})
+	});
+	describe('User Test Database', function(done){
+
+		User.collection.drop();
+
+		beforeEach(function(done){
+			var newUser = new User({
+				'username' : 'mohammad',
+			    'password' : 'testing', 
+			    'club' : 'DesertForce',
+			    'country' : 'Jordan'
+			});
+			newUser.save(function(err,savedUser){
+				done();
+
+			})
+		});
+		afterEach(function(done){
+		    User.collection.drop();
+			done();
+		});
+
+		it('should get all users in database', function(done){
+			chai.request(server)
+				.get('/api/users')
+				.end(function(err, res){
+					expect(res.status).to.be.equal(200);
+					expect(res.body.length).to.be.equal(1);
+					expect(res.body[0].username).to.be.equal('mohammad');
+					expect(res.body[0].password).to.be.equal(undefined);
+					done();
+				})
+		});
+
+		it('should return with status 500' , function(done){
+			User.collection.drop();
+			chai.request(server)
+				.get('/api/users')
+				.end(function(err, res){
+					expect(res.status).to.be.equal(500);
+					expect(res.body.length).to.be.equal(undefined);
+					done();
+				})
+		})
+
+		it('should get one user when username is passed in route' , function(done){
+			var newUser = new User({
+				'username' : 'super',
+			    'password' : '123', 
+			    'firstName' : 'Iron' ,
+			    'lastName' : 'Man',
+			    'club' : 'Makhai',
+			    'email' : 'ironman@avengers.com', 
+			    'country' : 'Jordan'
+			})
+			newUser.save(function(error , newUser){
+				chai.request(server)
+					.get('/api/user/'+ newUser.username)
+					.end(function(err, res){
+						expect(res.status).to.be.equal(200);
+						expect(res.body.username).to.be.equal('super');
+						expect(res.body).to.have.property('username');
+						expect(res.body).to.have.property('firstName');
+						expect(res.body).to.have.property('lastName');
+						expect(res.body).to.have.property('email');
+						done();
+					})
+				
+			})
+		})
+
+		it('should respond with status 500 Error if user is not available', function(done){
+			chai.request(server)
+				.get('/api/user/dontenter')
+				.end(function(err,res){
+					expect(res.status).to.be.equal(500);
+					done();
+				})
+		});
+
+		describe('Signing up in User Controller' ,function(done){
+			it('should have a method called signUp', function(done){
+				expect(typeof userController.signup).to.be.equal('function');
+				done();
+			});
+
+			it('return 500 if username already exists', function(done){
+				chai.request(server)
+					.post('/api/user/signup')
+					.send({
+						'username' : 'mohammad',
+					    'password' : 'testing', 
+					    'club' : 'DesertForce',
+					    'country' : 'Jordan'
+					})
+					.end(function(req,res){
+						expect(res.status).to.be.equal(500);
+						done();
+					})
+			});
+
+			it('should signup a new user', function(done){
+				chai.request(server)
+					.post('/api/user/signup')
+					.send({
+						'username' : 'Fighter',
+						'password' : 'fighting',
+						'club' : 'sourceMMA',
+						'country' : 'Jordan'
+					})
+					.end(function(err, res){
+						expect(err).to.be.equal(null);
+						expect(res.body.country).to.be.equal('Jordan');
+						expect(res.body).to.have.property('username');
+						expect(res.body).to.have.property('password');
+						expect(res.body).to.have.property('country');
+						expect(res.body).to.have.property('club');
+						done();
+					})
+			})
+
+			it('should return 500 Error if you forgot to add required keys', function(done){
+				chai.request(server)
+					.post('/api/user/signup')
+					.send({
+						'username' : 'ahmad',
+						'password' : 'ahmad',
+						'club' : 'makhai'
+					})
+					.end(function(err, res){
+						expect(res.status).to.be.equal(500);
+						done();
+					})
+			})
+		})
+
+
+		describe('Sign in User', function(done){
+			it('should have a method called singin', function(done){
+				expect(typeof userController.signin).to.be.equal('function');
+				done();
+			});
+
+			it('should response 500 ERROR if user is not available', function(done){
+				chai.request(server)
+					.post('/api/user/signin')
+					.send({
+						'username' :'amNotAvailable'
+					})
+					.end(function(err, res){
+						expect(err).to.not.equal(null);
+						expect(res.status).to.be.equal(500);
+						done();
+					})
+			});
+
+			it('should give access tokens when signin in', function(done){
+				chai.request(server)
+					.post('/api/user/signin')
+					.send({
+						'username' : 'mohammad', 
+						'password' : 'testing'
+					})
+					.end(function(err, res){
+						expect(res.body.token).to.not.equal(undefined);
+						expect(res.body).to.have.property('token');
+						done();
+					})
+			});
+
+			it('should return 500 ERROR if password is incorrect', function(done){
+				chai.request(server)
+					.post('/api/user/signin')
+					.send({
+						'username' : 'mohammad', 
+						'password' : 'notme'
+					})
+					.end(function(err, res){
+						expect(res.status).to.be.equal(500);
+						done();
+					})
+			})
+
+		})
+
+		describe('Checking Authentication in user Controller', function(done){
+			it('should have a method called checkAuth' , function(done){
+				expect(typeof userController.checkAuth).to.be.equal('function');
+				done();
+			});
+
+			it('should respond with status 500 ERROR if token is not available in headers', function(done){
+				chai.request(server)
+					.post('/api/user/signin')
+					.send({
+						'username' : 'mohammadsdf' ,
+						'password' : 'testing'
+					})
+					.end(function(err, response){
+						expect(response.header['x-access.token']).to.be.equal(undefined);
+						done();
+					})
+			})
+
+		})
+
+
+		// TODO User Test Database
+	});
+	describe('Club Test Database', function(done){
+		// TODO ClubTestDatabase 
+	});
+	describe('Tournaments DataBase', function(done){
+		// TODO Tournaments Database
+	});
 });
