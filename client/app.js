551612b133095d169f4abb3706d2e55144dabb54
Fatima Hammami <en.f.h@live.com>
2016-08-02 12:16:25 +0300

    ading tests

---
+++ client/app.js
@@ -0,0 +1,37 @@
+angular.module('zarad',[
+	'zarad.auth',
+	'ngRoute'
+])
+.config(function ($routeProvider , $httpProvider) {
+	$routeProvider
+	.when('/signin', {
+		templateURL : '',
+		controller : 'AuthController'
+	})
+	.when('/signup', {
+		templateURL : '',
+		controller : 'AuthController'
+	});
+
+	$httpProvider.interceptors.push('AttachTokens');
+})
+.factory('AttachTokens',function ($window){
+	var attach = {
+		request: function(object){
+			var jwt= $window.localStorage.getItem('com.zarad');
+			if(jwt){
+				object.headers['x-access-token']= jwt;
+			}
+			object.headers['Allow-Control-Allow-Origin']= '*';
+			return object;
+		}
+	};
+	return attach;
+})
+.run(function($rootScope, $location , Auth){
+	$rootScope.$on('$routeChangeStart',function(evt,next,current){
+		if(next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+			$location.path('/signin');
+		}
+	});
+});
