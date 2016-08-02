99d6cd45c2d8493d7b3d22122d34f07e51daf435
Mihyar <eng.mihyear@gmail.com>
2016-08-02 14:11:35 +0300

    adding new admin fucntion

--- server/Admin/adminController.js
+++ server/Admin/adminController.js
@@ -14,31 +14,33 @@ module.exports = {
 		})
 	},
 	//Add new admin 
-	
-	addAdmin : function (req,res) {
-		var username = req.body.username;
-		Admin.findOne({username:username})
-		.exec(function (error,admin) {
-			if(error){
-				res.status(500).send(error);
-			}else if(!admin){
-				var newAdmin = new Admin ({
-					username : req.body.username,
-					password : req.body.password,
-					email : req.body.email,
-					firstName :  req.body.firstName,
-					lastName : req.body.lastName
-				});
-				newAdmin.save(function (error,admin) {
-					if(error){
-						res.status(500).send(error);
-					}else{
-						res.status(200).send(newAdmin);
-					}
-				});
-			}else{
-				res.status(500).send("Admin Already Exists");
-			}
-		})
-	}
+
+	addAdmin: function (req, res) {
+    var username=req.body.username;
+    Admin.findOne({username: username})
+    .exec(function(error,admin){
+      if(error){
+        res.status(500).send(error);
+      } else if(!admin){
+      	console.log("YAAAAAAAAAAAA")
+        var newAdmin = new Admin ({
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName
+        });
+        
+        newAdmin.save(function(err, newAdmin){
+          if(err){
+            res.status(500).send(err);
+          } else {
+            res.status(200).send(newAdmin);
+          }
+        })
+      } else {
+        res.send(500,'Admin Already Exists');
+      }
+    })
+  }
 }
