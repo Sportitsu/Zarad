d33451fa95bec68138bcf258c63fc67431ad922d
Mihyar <eng.mihyear@gmail.com>
2016-08-02 23:16:35 +0300

    implementing addClub function

--- server/Club/clubController.js
+++ server/Club/clubController.js
@@ -8,7 +8,7 @@ module.exports ={
 		Club.findOne({username : username})
 		.exec(function (error,club) {
 			if(club){
-				returnClub = new Club ({
+				var returnClub = new Club ({
 					username : club.username,
 					country : club.country,
 					clubName : club.clubName
@@ -22,5 +22,30 @@ module.exports ={
 	// Add a new club
 	addClub : function(req,res){
 		var username = req.body.username;
+		Club.findOne({username : username})
+		.exec(function (error,club) {
+			if(!club){
+				var newClub = new Club({
+					username : req.body.username,
+					password : req.body.password,
+					country  : req.body.country,
+					clubName : req.body.clubName
+				});
+				newClub.save(function (error,club) {
+					if(error){
+						res.status(500).send(error);
+					}else{
+						var returnClub = new Club ({
+							username : club.username,
+							country : club.country,
+							clubName : club.clubName
+						})
+						res.status(201).send(returnClub)
+					}
+				})
+			}else{
+				res.status(500).send("Club Already Exists");
+			}
+		})
 	}
 }
