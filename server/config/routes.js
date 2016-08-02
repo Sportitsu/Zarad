6a1ae104a055475e2c6fd76ef8706eff98d15c91
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-02 10:51:43 +0300

    Added new routes to serve admin and home page

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,19 @@
 // Require all the controller functions here 
 
+// Waiting for database setup 
 
 module.exports = function(app,express){
 
 	app.get('/api/home' , function(req,res){
 		res.status(200).send('Connected to home');
+	});
+
+	app.get('/api/admin' , function(req,res){
+		res.status(200).send('Admin on road');
+	})
+
+	app.post('/api/admin' , function(req,res){
+		res.status(201)
 	})
 
 }
