0abf7f241a5b178d4b53be5e174399434d480038
Mihyar <eng.mihyear@gmail.com>
2016-08-02 12:00:34 +0300

    implement addAdmin function to add new admins

--- server/Admin/adminController.js
+++ server/Admin/adminController.js
@@ -4,12 +4,40 @@ var jwt = require('jwt-simple');
 module.exports = {
 	//fetch one admin
 	getAdmin : function (req, res){
-		Admin.findOne({username:req.body.username})
+		Admin.findOne({username:req.params.username})
 		.exec(function (error,admin) {
 			if(error){
 				console.error(error);
 			}else{
 				res.status(200).send(admin);
+			}
+		})
+	},
+	//Add new admin 
+	
+	addAdmin : function (req,res) {
+		var username = req.body.username;
+		Admin.findOne({username:username})
+		.exec(function (error,admin) {
+			if(error){
+				res.status(500).send(error);
+			}else if(!admin){
+				var newAdmin = new Admin ({
+					username : req.body.username,
+					password : req.body.password,
+					email : req.body.email,
+					firstName :  req.body.firstName,
+					lastName : req.body.lastName
+				});
+				newAdmin.save(function (error,admin) {
+					if(error){
+						res.status(500).send(error);
+					}else{
+						res.status(200).send(newAdmin);
+					}
+				});
+			}else{
+				res.status(500).send("Admin Already Exists");
 			}
 		})
 	}
