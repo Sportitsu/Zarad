70e4fa0b58354deab8f9f5a3c3232500cd38f7b0
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-02 14:06:00 +0300

    Added new test for Admin

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -1,19 +1,24 @@
+var should = require('chai').should();
 var expect = require ('chai').expect;
 var path = require('path')
 var supertest = require('supertest');
 var server = require(path.join(__dirname,'../../' ,'./server/server.js'));
 var chai = require('chai')
-      , chaiHttp = require('chai-http');
+      ,chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
 var request = supertest.agent(server);
 
 
+beforeEach(function(){
+	// TODO FIND FAKE POSTS AND DELETE THEM FROM DATABASE. 
+})
+
 describe("Server", function (){
 
 	describe('/GET' , function(done){
-		it("should respond with status 200 OK when routing to /api/home ", function (done){
+		it("is just for testing mocha and chai ", function (done){
 			chai.request(server)
 				.get('/api/home')
 				.end(function(err,res){
@@ -22,22 +27,79 @@ describe("Server", function (){
 					done();
 			})
 		});	
-	});
+	});		
+	describe('Admin Test Database', function(done){
 
-	describe('/POST',  function(done){
-		it('should respond with status 201 OK on route /api/admin' , function(done){
+		Admin.collection.drop();
+
+		beforeEach(function(done){
+			var newAdmin = new Admin({
+				'username' : 'admin-memf',
+			    'password' : '123', 
+			    'firstName' : 'testing' ,
+			    'lastName' : 'noneOfYourBusiness',
+			    'email' : 'notGonnaTellYou'
+			});
+			newAdmin.save(function(err,savedUser){
+				done();
+			})
+		});
+		afterEach(function(done){
+			Admin.collection.drop();
+			done();
+		});
+
+		it('should be an object with keys and values', function(done){
+			var testAdmin = new Admin({
+				'username' : 'super',
+			    'password' : '123', 
+			    'firstName' : 'Iron' ,
+			    'lastName' : 'Man',
+			    'email' : 'ironman@avengers.com'
+			});
+			testAdmin.save(function(err,data){	
+				 request.get('/api/admin/'+ data._id)
+						.set('Accept','application/json')
+						.expect(200)
+						.end(function(err, res){
+							expect(res.body).to.have.property('username');
+							expect(res.body.username).to.not.equal(null);
+							expect(res.body).to.have.property('firstName');
+							expect(res.body.firstName).to.not.equal(null);
+							expect(res.body).to.have.property('lastName');
+							expect(res.body.lastName).to.not.equal(null);
+							expect(res.body).to.have.property('email');
+							expect(res.body.email).to.not.equal(null);
+							done();
+						});
+			})
+		})
+
+
+
+		it('should update admin with a new one with response 201 OK' , function(done){
 			chai.request(server)
 				.post('/api/admin')
+				.set('Accept','application/json')
 				.send({
 						'username' : 'admin-memf',
-					    'password' : '123'
+					    'password' : '123', 
+					    'firstName' : 'testing' ,
+					    'lastName' : 'noneOfYourBusiness',
+					    'email' : 'notGonnaTellYou'
 					})
 				.end(function(err,res){
 					expect(err).to.be.null;
 					expect(res.status).to.be.equal(201);
+					expect(res.body.username).to.be.equal('admin-memf');
+					expect(res.body.password).to.be.equal(null);
+					expect(res.body.firstName).to.be.equal('testing');
+					expect(res.body.lastName).to.be.equal('noneOfYourBusiness');
+					expect(res.body.email).to.be.equal('notGonnaTellYou');
 					done();
 				})
-		})
-	})
+		});
 
+
+	})
 });
