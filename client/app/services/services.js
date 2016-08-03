7d064b159c36b73d1254d96a0e2c4053ed272ef4
elham-rababah <elham-rababah@yahoo.com>
2016-08-03 18:52:26 +0300

    test

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,22 +2,27 @@
 angular.module('zarad.services', [])
 .factory('Auth', function ($http, $location, $window) {
   var signin = function (user) {
+    console.log(user);
     var ObjUser={
       method: 'POST',
       data: user
     }
-    if(user[type]==="player"){
+    // this if statement to determain the type of user 
+    if(user['type']==="player"){
       ObjUser["url"]='/api/user/signin'
     }
-     if(user[type]==="admin"){
+     if(user['type']==="admin"){
       ObjUser["url"]=''
     }
-     if(user[type]==="club"){
+     if(user['type']==="club"){
       ObjUser["url"]=''
     }
    return $http(ObjUser)
     .then(function (resp) {
-      return resp.data.token;
+      return {token:resp.data.token,
+        path:resp.data.path
+      }
+
     });
   };
 
@@ -32,17 +37,19 @@ angular.module('zarad.services', [])
   };
 })
 
-
 .factory('Admin', function ($http, $location, $window) {
+  //send club information to server
   var Addclub=function(club){
+    console.log(club)
     return $http({
       method:'POST',
       data: club,
-      url:''
+      url:'/api/clubregister'
 
     })
 
   };
+   //send club information to tournament
   var Addtournament=function(tournament){
     return $http({
       method:'POST',
