40d316f3986b365c4bfa6a494eb2cda203536d56
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-04 09:14:12 +0300

    Comon TRAVIS

--- server/server.js
+++ server/server.js
@@ -1,29 +1,28 @@
-var mongoose = require('mongoose');
-var express = require('express');
-var config = require('./_config');
+// var mongoose = require('mongoose');
+// var express = require('express');
+// var config = require('./_config');
 
-var app = express();
+// var app = express();
 
-// connect to mongoDB database
-// var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/zarad';
-var port = process.env.PORT || 8000;
+// // connect to mongoDB database
+// var port = process.env.PORT || 8000;
 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.mongoURI[app.settings.env],function(err, res){
-	if(err){
-		console.log('Error Connecting to the database. ' + err);
-	} else {
-		console.log('Connected to Database ' + config.mongoURI[app.settings.env])
-	}
-});
+// mongoose.Promise = global.Promise;
+// mongoose.connect(config.mongoURI[app.settings.env],function(err, res){
+// 	if(err){
+// 		console.log('Error Connecting to the database. ' + err);
+// 	} else {
+// 		console.log('Connected to Database ' + config.mongoURI[app.settings.env])
+// 	}
+// });
 
-// configure server with all routing plus middleware
-require('./config/middleware.js')(app,express);
-require('./config/routes.js')(app, express);
+// // configure server with all routing plus middleware
+// require('./config/middleware.js')(app,express);
+// require('./config/routes.js')(app, express);
 
-// start listening on port 8000
-var listener = app.listen(port , function(){
-	console.log('Listening on port ' + listener.address().port); // Listening port
-})
+// // start listening on port 8000
+// // var listener = app.listen(port , function(){
+// // 	console.log('Listening on port ' + listener.address().port); // Listening port
+// // })
 
-module.exports = app;
+// // module.exports = app;
