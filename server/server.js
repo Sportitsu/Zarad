dc48c8fd89ae526d2d9e1a2d44c5aac131b79425
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-06 20:23:10 +0300

    Fixed config Issue was interfering with database from heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,13 @@ var app = express();
 var MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/node-test';
 // connect to mongoDB database
 var port = process.env.PORT || 8000;
+// mongoose.connect(config.mongoURI[app.settings.env],function(err){
+// 	if(err){
+// 		console.log('Error Connecting to the database. ' + err);
+// 	} else {
+// 		console.log('Connected to Database ' + config.mongoURI[app.settings.env]);
+// 	}
+// });
 mongoose.connect(MONGO_URI);
 
 // configure server with all routing plus middleware
