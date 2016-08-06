79bb9f727c33365c57cd5cda20a79fc677378514
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-06 16:00:44 +0300

    Fixed typos for jshint

--- server/_config.js
+++ server/_config.js
@@ -3,6 +3,6 @@ var config = {};
 config.mongoURI = {
 	development : process.env.MONGODB_URI || 'mongodb://localhost/zarad' ,
 	test : 'mongodb://localhost/node-test'
-}
+};
 
 module.exports = config;
