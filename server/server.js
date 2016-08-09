4e04caf215e3badd532b0ae52cec344e680492b2
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-09 15:24:45 +0300

    Adding better headers for allowing cross origin

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ mongoose.connect(MONGO_URI);
 var app = express();
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Accept');
-    next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Headers');
+	next();
 });
 
 
