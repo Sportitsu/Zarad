9cdee0da34aa47c30e0c77552cfcb9890036277a
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-09 10:44:14 +0300

    Fixed clock for the new heroku schedular

--- server/config/clock.js
+++ server/config/clock.js
@@ -1,26 +1,20 @@
-var CronJob = require('cron').CronJob;
 var User = require('./../User/userModel');
-
-module.exports = {
-	job : new CronJob('00 27 5 * * *', function(){
-		var threeDays = 259200000;
-		var oneMonth  = 2592000000;
-		User.find({}).exec(function(err, users){
-				for(var i = 0 ; i < users.length; i++){
-						var isFinished = users[i].subscription + ( oneMonth * users[i].membership);
-						if(isFinished - Date.now() < threeDays && isFinished - Date.now() > -1){
-							console.log('Should Subscribe');
-							users[i].resub = true;
-						} else if(isFinished - Date.now() > 0){
-							console.log('Still Valid , Good To Go');
-							users[i].valid = true;
-						 } else {
-						 	console.log('Sorry , better come again soon to run');
-						 	users[i].valid = false;
-							users[i].resub = true;
-						 }
-						 users[i].save();
-				}
-			});
-	}, null, true, 'Asia/Amman')
-}
+var mongoose = require('mongoose');
+var MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/node-test';
+mongoose.connect(MONGO_URI);
+var threeDays = 259200000;
+var oneMonth  = 2592000000;
+User.find({}).exec(function(err, users){
+	for(var i = 0 ; i < users.length; i++){
+			var isFinished = users[i].subscription + ( oneMonth * users[i].membership);
+			if(isFinished - Date.now() < threeDays && isFinished - Date.now() > -1){
+				users[i].resub = true;
+			} else if(isFinished - Date.now() > 0){
+				users[i].valid = true;
+			 } else {
+			 	users[i].valid = false;
+				users[i].resub = true;
+			 }
+			 users[i].save();
+	}
+});
