52cca0b0b8884710e32c99bcc00fe4d2bd7e5919
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-09 05:00:43 +0300

    Took out cron from helpers to not crash our app with heroku

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,31 +1,7 @@
 'use strict';
 var jwt = require('jwt-simple');
-var CronJob = require('cron').CronJob;
-var User = require('./../User/userModel');
-module.exports = {
-	job : new CronJob('00 00 00 * * *', function(){
-		console.log('starting');
-		var threeDays = 259200000;
-		var oneMonth  = 2592000000;
-		User.find({}).exec(function(err, users){
-				for(var i = 0 ; i < users.length; i++){
-						var isFinished = users[i].subscription + ( oneMonth * users[i].membership);
-						if(isFinished - Date.now() < threeDays && isFinished - Date.now() > -1){
-							console.log('Should Subscribe');
-							users[i].resub = true;
-						} else if(isFinished - Date.now() > 0){
-							console.log('Still Valid , Good To Go');
-							users[i].valid = true;
-						 } else {
-						 	console.log('Sorry , better come again soon to run');
-						 	users[i].valid = false;
-							users[i].resub = true;
-						 }
-						 users[i].save();
-				}
-			});
-	}, null, true, 'Asia/Amman'),
 
+module.exports = {
 	getClubName : function(clubName){
 
 		var name = 'cl';
