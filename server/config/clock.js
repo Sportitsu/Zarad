4bb23f081270e4d1407236a7d1663a870298c71d
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-09 11:14:27 +0300

    Testing heroku

--- server/config/clock.js
+++ server/config/clock.js
@@ -1,10 +1,19 @@
 var User = require('./../User/userModel');
 var mongoose = require('mongoose');
 var MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/node-test';
-
+// var time = require('time');
 mongoose.connect(MONGO_URI);
 var threeDays = 259200000;
 var oneMonth  = 2592000000;
+
+// var a = new time.Date(59837089475);
+// console.log(a.toString());
+
+// var now = new time.Date(Date.now());
+// console.log(now.toString());
+	console.log('Hello Guys');
+// console.log('This is the difference between the two  ' +( (59837089475 + oneMonth) - Date.now()) );
+
 User.find({}).exec(function(err, users){
 	for(var i = 0 ; i < users.length; i++){
 			var isFinished = users[i].subscription + ( oneMonth * users[i].membership);
