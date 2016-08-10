c4076014350b555d1f614f22854d67dbd249f79d
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-10 21:04:35 +0300

    Displaying all user profile details

--- www/js/controller/userProfile.js
+++ www/js/controller/userProfile.js
@@ -1,5 +1,67 @@
 angular.module('zarad.user',['ionic'])
-.controller('UserProfileController', function($scope, Auth, $location, $window, $ionicPlatform){
- console.log($window.localStorage.getItem('com.user'));
- console.log($window.localStorage.getItem('com.zarad'));
-});
+.controller('UserProfileController',
+ function($scope, $ionicPopup, $state, Auth, $location, $window, $ionicPlatform, User, $ionicLoading, $timeout){
+ $scope.user = {};
+ $scope.data = {};
+ $scope.flag = false;
+
+  $scope.showConfirm = function() {
+   var confirmPopup = $ionicPopup.confirm({
+     title: 'Are you sure of your edit',
+     template: 'Please be sure'
+   });
+
+   confirmPopup.then(function(res) {
+     if(res) {
+     	$scope.isChecked = false;
+     	$scope.flag = false;
+     	$scope.confirm();
+     } else {
+       console.log('You are not sure');
+     }
+   });
+ };
+
+  $ionicLoading.show({
+    content: 'Loading',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 200,
+    showDelay: 0
+  });
+
+  $timeout(function () {
+    $ionicLoading.hide();
+	 console.log($window.localStorage.getItem('com.zarad'));
+	 User.getUser($window.localStorage.getItem('com.user'))
+	 	.then(function(data){
+	 		$scope.data = data.data;
+	 		console.log(data.data);
+	 	})
+	 	.catch(function(error){
+	 		console.log(error);
+	 	});
+  }, 1000);
+
+
+ $scope.edit = function(){
+  if($scope.isChecked){
+  	$scope.flag = true;
+  } else {
+  	$scope.flag = false;
+  }
+ }
+
+ $scope.confirm = function(){
+ 	$scope.user.username = $scope.data.username;
+ 	User.editProfile($scope.user)
+ 		.then(function(response){
+ 			$scope.data = response.data;
+ 		})
+ 		.catch(function(error){
+ 			console.log(error);
+ 		})
+	$state.go($state.current, {}, {reload: true});
+ }
+
+})
