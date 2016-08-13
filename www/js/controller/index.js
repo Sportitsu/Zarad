2ed23f67e8906e72502ef1f6be79c9b7683684c7
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-13 15:02:17 +0300

    Added new popup that shows when membership ends in action sheet

--- www/js/controller/index.js
+++ www/js/controller/index.js
@@ -5,7 +5,7 @@ angular.module('zarad.index',['ionic'])
 	//signout function
 	$scope.user = {};
 	$scope.data = JSON.parse(window.localStorage.user)
-// Gets the Date of Today to Use in Membership Action PopUp	
+	// Gets the Date of Today to Use in Membership Action PopUp	
 	var objToday = new Date(),
 	  weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
 	  dayOfWeek = weekday[objToday.getDay()],
@@ -57,7 +57,17 @@ angular.module('zarad.index',['ionic'])
 	};
 
 	$scope.showDate = function(){
-		console.log('TO DO THIS FUNCTION');
+			var willFinish = new Date($scope.data.subscription+((30*24*60*60*1000)*$scope.data.membership));
+			willFinish+='';
+			willFinish = willFinish.substr(0,16);
+		var myPopup =  $ionicPopup.alert({
+			title : '<b style="color:red">' +today + '</b>',
+			template : 'Your membership ends in <b style="color:red">' + willFinish + '</b>'
+		});
+
+		myPopup.then(function(res){
+			console.log('Done');
+		})
 	};
 	
 	$scope.logout=function(){
