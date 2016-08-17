9179a410845b882453e0580a15f952b486322fe0
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-17 12:25:01 +0300

    Done with displaying video profile and passing the right channel ID

--- www/js/controller/User/videoProfile.js
+++ www/js/controller/User/videoProfile.js
@@ -2,39 +2,45 @@ angular.module('zarad.videos', ['ionic'])
 .controller('VideosController', function($scope, User, Auth, Club, $location, $http){
 
   $scope.data = JSON.parse(window.localStorage.getItem('user'));
-
   Club.getClubForUser({clubName : $scope.data.club})
       .then(function(response){
-        console.log(response.data);
+        $scope.flag = true;
+        $scope.myClub = response.data;
+        $scope.uploadPage();
       })
       .catch(function(error){
-        console.log(error);
-      })
-
-	$scope.playerVars = {
-	  rel: 0,
-	  showinfo: 0,
-	  modestbranding: 0,
-	}
-
-  var YOUTUBE_API_KEY2 = 'AIzaSyCe3SQ2EGPUgyqeW5PXscj8i4cN47Sck8Y';
-
-  $scope.youtubeParams = {
-      key: YOUTUBE_API_KEY2,
-      type: 'video',
-      maxResults: '5',
-      part: 'id,snippet',
-      q: '',
-      order: 'date',
-      channelId: 'UCet7ZVoSfu1QWwlVxua12Gg',
-    }
-
-	$scope.videos = [];
-      $http.get('https://www.googleapis.com/youtube/v3/search', {params:$scope.youtubeParams}).success(function(response){
-      angular.forEach(response.items, function(child){
-        console.log(child);
-        $scope.videos.push(child);
+        $scope.flag = false;
+        // console.log(error);
       });
-    })
+
+  console.log($scope.data.club);
+
+$scope.uploadPage = function(){
+  	$scope.playerVars = {
+  	  rel: 0,
+  	  showinfo: 0,
+  	  modestbranding: 0,
+  	}
+
+    var YOUTUBE_API_KEY2 = 'AIzaSyCe3SQ2EGPUgyqeW5PXscj8i4cN47Sck8Y';
+
+    $scope.youtubeParams = {
+        key: YOUTUBE_API_KEY2,
+        type: 'video',
+        maxResults: '5',
+        part: 'id,snippet',
+        q: '',
+        order: 'date',
+        channelId: $scope.myClub.channelId,
+      }
+
+  	$scope.videos = [];
+        $http.get('https://www.googleapis.com/youtube/v3/search', {params:$scope.youtubeParams}).success(function(response){
+        angular.forEach(response.items, function(child){
+          $scope.videos.push(child);
+        });
+      })
+}
+
     
 })
