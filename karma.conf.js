e37dc4b39ebf46e16ee3028ae903ee43cbb0b321
Mihyar <eng.mihyear@gmail.com>
2016-08-18 10:19:47 +0300

    fixing reporter

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'www/js/*.js' : 'coverage'
+        'www/js/**/*.js' : 'coverage'
     },
 
 
