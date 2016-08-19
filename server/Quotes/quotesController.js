633c7e1a528a8a91be57cc985b7f8a06907fe283
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-19 18:29:51 +0300

    New Quote model in the backend

---
+++ server/Quotes/quotesController.js
@@ -0,0 +1,1 @@
+quotesController.js
---
+++ server/Quotes/quotesModel.js
@@ -0,0 +1,12 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var quoteSchema = new Schema({
+ image : { type : String};
+});
+
+var Quote = mongoose.model('Quote', quoteSchema);
+
+module.exports = Quote;
