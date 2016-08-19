ee23d80100919315d003f1110bd7d9bbea24872d
Mihyar <eng.mihyear@gmail.com>
2016-08-19 15:38:10 +0300

    commenting the previous tests and start from scratch

--- test/front-end/ServicesSpec.js
+++ test/front-end/ServicesSpec.js
@@ -1,205 +1,60 @@
 'use strict';
 
-describe('AuthController', function () {
-  var $scope, $rootScope, $location, $window, $httpBackend, createController, Auth;
+describe('Services', function () {
+  var baseUrl = 'http://zarad.herokuapp.com';
+  // Before each test load our lets-hangout.services module
+  beforeEach(angular.mock.module('zarad.services'));
 
-  // using angular mocks, we can inject the injector
-  // to retrieve our dependencies
-  beforeEach(module('zarad'));
-  beforeEach(inject(function ($injector) {
-
-    // mock out our dependencies
-    $rootScope = $injector.get('$rootScope');
-    $location = $injector.get('$location');
-    $window = $injector.get('$window');
-    $httpBackend = $injector.get('$httpBackend');
-    Auth = $injector.get('Auth');
-    $scope = $rootScope.$new();
-
-    var $controller = $injector.get('$controller');
-
-    // used to create our AuthController for testing
-    createController = function () {
-      return $controller('AuthController', {
-        $scope: $scope,
-        $window: $window,
-        $location: $location,
-        Auth: Auth
-      });
-    };
-
-    createController();
-  }));
-
-  afterEach(function () {
+  afterEach(inject(function ($httpBackend) {
     $httpBackend.verifyNoOutstandingExpectation();
     $httpBackend.verifyNoOutstandingRequest();
-    $window.localStorage.removeItem('com.zarad');
+  }));
+
+  describe('Auth factory', function() {
+    var $httpBackend, Auth;
+
+    // Before each test set our injected Auth factory (_Auth_) to our local Users variable
+    beforeEach(inject(function(_$httpBackend_, _Auth_) {
+      Auth = _Auth_;
+      $httpBackend = _$httpBackend_;
+    }));
+
+    // A test to verify the Auth factory exists
+    it('Auth factory should exist', function() {
+      expect(Auth).toBeDefined();
+    });
+    
+    describe('.signup()', function() {
+      // A test to verify the method signup exists
+      it('signup should exist', function() {
+        expect(Auth.signup).toBeDefined();
+      });
+
+      it('signup should POST data and username should start with Pl', function() {
+        var mockResponse = 
+          {
+            'username':'Plmiha492',
+            'password': 'play',
+            'firstName' : 'Mihyar',
+            'lastName' : 'almaslama',
+            'country' : 'Syria',
+            'email' : 'mihyar@gmail.com',
+            'status' : 201
+          }
+
+        $httpBackend.expect('POST', baseUrl + '/api/user/signup').respond(mockResponse);
+
+        Auth.signup().then(function (resp) {
+          expect(resp.status).toEqual(201);
+          expect(resp.username[0]).toEqual('P');
+          expect(resp.username[1]).toEqual('l');
+        });
+        $httpBackend.flush();
+      });
+
+      it('should return token when a user signin', function(){
+        var token = ''
+      })
+    });
   });
-
-      it('should exist', function(){
-      expect(Auth).to.exist;
-    });
-
-    it('should have a method signup', function(){
-      expect(Auth.signup).to.be.a('function')
-    });
-
-    it('should have a method signin', function(){
-      expect(Auth.signin).to.be.a('function')
-    });
-
-    it('should have a method signout', function(){
-      expect(Auth.signout).to.be.a('function')
-    });
-
-    it('should have a method isAuth', function(){
-      expect(Auth.isAuth).to.be.a('function')
-    });
-
-    it('should have a method checkUser', function(){
-      expect(Auth.checkUser).to.be.a('function')
-    });
-
-})
-
-//======================================
-
-
-
-//======================================
-// describe('Services', function () {
-// beforeEach(module('zarad'));
-
-//   afterEach(inject(function ($httpBackend) {
-//     // $httpBackend.verifyNoOutstandingExpectation();
-//     $httpBackend.verifyNoOutstandingRequest();
-//   }));
-
-//   describe('Factory', function () {
-//   var $httpBackend, Admin;
-
-//    beforeEach(inject(function (_$httpBackend_, _Admin_, _Club_, _Tournament_, _User_, _Auth_, _$window_) {
-//       $httpBackend = _$httpBackend_;
-//       Admin = _Admin_;
-//       Club = _Club_;
-//       Tournament = _Tournament_;
-//       User = _User_;
-//       Auth = _Auth_;
-//       $window = _$window_;
-//     }));
-
-//    it('should exist', function () {
-//       expect(Admin).to.exist;
-//     });
-
-//    it('should have a method `Addclub`', function () {
-//       expect(Club.Addclub).to.be.a('function');
-//     });
-
-//   it('should add a new club with `Addclub`', function () {
-//       var club = {
-//         username:1,
-//         password:"1111",
-//         country:"jordan",
-//         clubName: "zarad jordan" };
-
-//       $httpBackend
-//         .expect('POST','http://zarad.herokuapp.com/api/club/register')
-//         .respond(201, {
-//         username:1,
-//         password:"1111",
-//         country:'jordan',
-//         clubName: "zarad jordan"
-//         });
-      
-//       Club.Addclub(club).then(function (resp) {
-//         expect(resp.status).to.equal(201);
-//         expect(resp.data.clubName).to.equal("zarad jordan");
-//       });
-//     });
-
-//     // test Addtournament 
-//    it('should have a method `AddTournament`', function () {
-//       expect(Tournament.AddTournament).to.be.a('function');
-//     });
-
-//   it('should add a new tournament with `AddTournament`', function () {
-//       var tournament  = {
-//         name:"cup",
-//         Date:"1111",
-//         place:"jordan",
-//         organizer: "zarad jordan",
-//         details:" jejestu cup in jordan" ,
-//         poster:"tournament.gpj"
-//     };
-
-//       $httpBackend
-//         .expect('POST','http://zarad.herokuapp.com/api/tournament/create')
-//         .respond(201, {
-//         name:"cup",
-//         Date:"1111",
-//         place:"jordan",
-//         organizer: "zarad jordan",
-//         details:" jejestu cup in jordan" ,
-//         poster:"tournament.gpj"
-//         });
-
-//       Tournament.AddTournament(tournament ).then(function (resp) {
-//         expect(resp.status).to.equal(201);
-//         expect(resp.name).to.equal('cup');
-//       });
-//     });
-//   ///////////////////////////////
-//    it('User should exist', function () {
-//       expect(User).to.exist;
-//     });
-
-//    it('Club should have a method `getClub`', function () {
-//       expect(Club.getClub).to.be.a('function');
-//     });
-
-//     it('should get club with `getClub`', function () {
-//        var club = {
-//         username:1,
-//         password:"1111",
-//         country:"jordan",
-//         clubName: "zarad jordan" };
-
-//       $httpBackend.expect('GET', 'http://zarad.herokuapp.com/api/club/x/1').respond(club);
-
-//       Club.getClub(1).then(function (club) {
-//         expect(club.data).to.deep.equal(club);
-//       });
-//     });
-//   ///////////////////////////////
-//     it('should exist', function () {
-//       expect(Auth.signup).to.be.a('function');
-//     });
-
-//     it('should signup a new user', function () {
-//       var user = {
-//         password : 'test',
-//         country : 'Syria',
-//         beltColor : 'purple',
-//         club : 'Makhai',
-//         membership   : 1,
-//         firstName : "mihyar",
-//         email : "mihyar@gmail.com"
-//       };
-//       $httpBackend.expect('POST','http://zarad.herokuapp.com/api/signup').respond(201, user);
-//       Auth.signup(user)
-
-//     });  
-      
-//       it('should be able to signin', function(){
-//         var user = {
-//           username : 'Plmoha492',
-//           password : 'test'
-//         }
-//         $httpBackend.expectPOST('http://zarad.herokuapp.com/api/user/signin').respond(user);
-//         Auth.signin()
-//       })
-
-//   });
-// });
+});
