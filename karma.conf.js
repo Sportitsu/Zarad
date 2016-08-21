5693d74d95c5e265b6b8bd06c0a177e37f8d4616
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-21 11:23:49 +0300

    Commented paths to javascript files

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
       'www/ionic/js/angular/angular.js',
       'www/lib/angular-mocks/angular-mocks.js',
       'www/lib/ionic/js/ionic.js',
+      'www/lib/angular-youtube-mb/dist/angular-youtube-embed.min.js',
       'www/lib/ionic/js/ionic-angular.js',
       'www/lib/ionic/js/ionic.bundle.js',
       'www/ionic/js/ionic.bundle.js',
@@ -30,9 +31,20 @@ module.exports = function(config) {
       'www/lib/ionic/js/ionic.bundle.js',
       'www/app.js',
       'www/js/**/*.js',
+      // 'www/js/controller/auth.js',
+      // 'www/js/services/services.js',
+      // 'www/js/controller/User/userProfile.js',
+      // 'www/js/controller/User/userHome.js',
+      // 'www/js/controller/User/videoProfile.js',
+      // 'www/js/controller/AdminHome.js',
+      // 'www/js/controller/club.js',
+      // 'www/js/controller/tournament.js',
+      // 'www/js/controller/index.js',
+      // 'www/js/controller/ng-cordova.min.js',
       // 'test/front-end/AdminSpec.js',
       //'test/front-end/routingSpec.js',
       'test/front-end/ServicesSpec.js',
+      'test/front-end/profileSpec.js'
       //'test/front-end/profileSpec.js',
       //'test/front-end/clubSpec.js'
     ],
@@ -65,6 +77,10 @@ module.exports = function(config) {
     // web server port
     port: 9876,
 
+
+    jasmineNodeOpts: {
+        isVerbose: true,
+    },
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
