ec22e43c210039bb80d85dfda6b5c6159dffb222
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-08-22 13:58:54 +0300

    before merge

--- test/front-end/clubSpec.js
+++ test/front-end/clubSpec.js
@@ -1,42 +1,191 @@
-describe('clubController',function(){
-  var  $scope, $rootScope, $window,$location,club,createController,$httpBackend;
-  beforeEach(module('zarad'))
-  beforeEach(inject(function($injector){
-       
-    $rootScope=$injector.get('$rootScope');
-    $window=$injector.get('$window');
-    $location=$injector.get('$location');
-    $httpBackend=$injector.get('$httpBackend')
-    club=$injector.get('club');
-    $scope=$rootScope.$new();
-    
-    var $controller=$injector.get('$controller');
-        
-    createController = function(){
-      return $controller('clubController',{
-        $scope: $scope,
-        $window: $window,
-        $location : $location,
-        club : club
-      });
-    };
-    
-    createController();
+'use strict';
+
+describe('Club Controller', function(){
+  beforeEach(angular.mock.module('zarad.club'));
+  beforeEach(angular.mock.module('zarad.services'));
+  beforeEach(angular.mock.module('ionic'));
+
+
+
+  var $scope , $window , $q , deferred , $location , User, Auth, Club, ionicPopupMock , ionicActionMock , ionicModalMock;
+  beforeEach(inject(function($rootScope, $controller, _$window_, _$location_, _$q_, _User_, _Auth_, _Club_, $ionicPopup, $ionicModal, $ionicActionSheet, _$timeout_ ){
+
+    $scope = $rootScope.$new();
+    $window = _$window_;
+    $q = _$q_;
+    deferred = $q.defer();
+    $location = _$location_;
+    User = _User_;
+    Auth = _Auth_;
+    ionicPopupMock = $ionicPopup;
+    ionicModalMock = $ionicModal;
+    ionicActionMock = $ionicActionSheet;
+    Club = _Club_;
+
+
+    spyOn(_Club_, 'editClub').and.returnValue(deferred.promise);
+    spyOn(_Club_, 'AddUser').and.returnValue(deferred.promise);
+    spyOn(_Club_, 'getClub').and.returnValue(deferred.promise);
+    spyOn(_Club_, 'getClubUsers').and.returnValue(deferred.promise);
+    spyOn(_User_, 'resub').and.returnValue(deferred.promise);
+
+    $controller('clubController', {
+      $scope : $scope, 
+      $window : $window,
+      Auth : Auth ,
+      Club : Club ,
+      User : User ,
+      $ionicPopup : ionicPopupMock ,
+      $ionicModal : ionicModalMock , 
+      $ionicActionSheet : ionicActionMock ,
+      $timeout : _$timeout_
+    });
+
+    $window.localStorage['user'] = 'Clmsadk123';
+
   }));
 
-  afterEach(function () {
-   $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
-     });
-  console.log($scope)
-  it('should have a AddUser method',function(){
-    expect($scope.AddUser).to.be.a('function');
+  it('should istantiate Club controller', function(){
+    expect($scope.clubUsers).toBeDefined();
   });
-  it('should be able to create new AddUser with AddUser()', function () {
-    $httpBackend.expectPOST("http://zarad.herokuapp.com/api/club/register").respond(201, '');
-    //$httpBackend.whenGET('/api/user/signup').respond({status: 200});
-      club.AddUser();
-      $httpBackend.flush();
-      expect(res.status).toEqual(201);
-  });
+
+  it('should show Action sheet when calling show action function', inject(function($q) {
+    var popup = spyOn(ionicActionMock, 'show').and.callThrough().and.returnValue(deferred.promise);
+    $scope.showClubAction();
+    expect(popup).toHaveBeenCalled();
+    expect(true).toBe(true);
+  }));
+
+  it('should have a function called show User', function(){
+    expect($scope.showUser).toBeDefined();
   })
+
+  it('should have show User working', function(){
+    $scope.userProfileData;
+    $scope.showUser('Some Data Here');
+    expect($scope.userProfileData).toEqual('Some Data Here');
+  });
+
+
+  it('should have show User working', function(){
+    $scope.userProfileData;
+    $scope.editUser('Some Data Here');
+    expect($scope.userProfileData).toEqual('Some Data Here');
+  });
+
+  it('should show Popup When Confirming club edit', inject(function($q) {
+    var popup = spyOn(ionicPopupMock, 'confirm').and.callThrough().and.returnValue(deferred.promise);
+    $scope.confirmClubEdit();
+    expect(popup).toHaveBeenCalled();
+    expect(true).toBe(true);
+  }));
+
+
+  // it('should return promise when editing club', function(){
+
+  //   deferred.resolve({ image : 'sdfsdf.jpg'})
+  //   $scope.club = { data : { image : 'sdfsdf.jpg'}};
+  //   $scope.editClub();
+  //   $scope.$apply();
+
+  //   expect(Club.editClub).toHaveBeenCalled();
+
+  // });
+
+  // it('should be able to add user', function(){
+
+  //   deferred.resolve({username : 'Plffhu123'});
+  //   $scope.club = {data : {clubName: 'Makhai'}};
+
+  //   $scope.AddUser();
+  //   $scope.$apply();
+
+  //   expect(User).toBeDefined();
+
+  // });
+
+  it('should show when there are unsubscribed users', function(){
+    $scope.usersToSubscribe = [1,2,3];
+    $scope.show();
+    $scope.$apply();
+    expect(true).toBe(true); 
+  });
+
+  it('should return false when no unsubscribed users', function(){
+    $scope.usersToSubscribe;
+    $scope.show();
+    $scope.$apply();
+    expect(true).toBe(true);
+  });
+
+
+  it('should get Club when get Club function is called', function(){
+    
+    $scope.club = {data: ''};
+    deferred.resolve({username : 'Clmajkh34' , image : '23424iandf.jpg' , clubName: 'TestingClub', data:[{subscription:'12323'},{subscription:'231'}]});
+    $scope.getClub();
+    $scope.$apply();
+    expect(true).toBe(true);
+  })
+
+
+  it('should get Club when get Users function is called', function(){
+    $scope.club = {data : ''}
+    $scope.clubUsers = { data : [] }  ;
+    deferred.resolve({data:[{subscription: '14089'}, {subscription : '1234'} , {subscription :'234'}, {subscription: '2433242'}]});
+    $scope.getUsers();
+    $scope.$apply();
+
+    expect(Club.getClubUsers).toHaveBeenCalled();
+    expect(true).toBe(true);
+  });
+
+  it('should have a method that checks subscription', function(){
+
+    $scope.usersToSubscribe = { data : []};
+    $scope.usersEndedSubs = { data : []};
+    var object= { resub : true , subscription : '1234382', membership : '1' , valid: true};
+    $scope.checkSubscription(object);
+    $scope.$apply();
+    expect(true).toBe(true);
+  });
+
+
+  it('should have a popup resup function', function(){
+    var popup = spyOn(ionicPopupMock, 'show').and.callThrough().and.returnValue(deferred.promise);
+    $scope.resup();
+    expect(popup).toHaveBeenCalled();
+    expect(true).toBe(true);
+  });
+
+
+  it('should have a function that resubscribes users' , function(){
+    $scope.club = { data : { clubName: 'testingClub'}};
+    deferred.resolve({data:[{subscription: '14089'}, {subscription : '1234'} , {subscription :'234'}, {subscription: '2433242'}]});
+    $scope.renew('anyname');
+    $scope.$apply();
+
+    expect(true).toBe(true);
+  });
+
+  it('should have a function that gets time', function(){
+    // TODO this TEST 
+    // dont know what is going on 
+    $scope.getTime();
+    $scope.$apply();
+    expect(true).toBe(true);
+  })
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
--- test/front-end/profileSpec.js
+++ test/front-end/profileSpec.js
@@ -5,24 +5,31 @@ describe('Testing AngularJS Zarad Profile Page', function(){
     beforeEach(angular.mock.module('ionic'));
 
     describe('Testing parentController', function(){
-      var scope , ctrl, $window;
-      var $q , deferred, User;
-      beforeEach(inject(function($controller, $rootScope, _$window_, _$q_, _User_, _$timeout_){
+      var scope , $window, ionicPopupMock , ionicActionMock;
+      var $q , deferred, User, Auth;
+      beforeEach(inject(function($controller, $rootScope, _$window_, _$q_, _User_, _$timeout_, $ionicPopup, $ionicActionSheet, _Auth_){
         scope = $rootScope.$new();
-        ctrl = $controller('parentController',{$scope : scope});
         $window = _$window_;
         $window.localStorage.member=JSON.stringify({beltColor : 'purple'});
         $q =  _$q_;
         deferred = _$q_.defer();
         User = _User_;
-        spyOn(_User_, 'editProfile').and.returnValue(deferred.promise);
+        Auth = _Auth_;
+
+        spyOn(User, 'editProfile').and.returnValue(deferred.promise);
+        spyOn(Auth, 'signout').and.callThrough();
+
+        ionicPopupMock = $ionicPopup;
+        ionicActionMock = $ionicActionSheet;
 
         $controller('parentController', { 
           $scope: scope, 
-          User: _User_, 
+          User: _User_,
+          Auth : Auth, 
           $window: _$window_,
           $timeout: _$timeout_, 
-          $q: _$q_ 
+          $q: _$q_ ,
+          $ionicPopup : ionicPopupMock
         });
       }));
 
@@ -59,12 +66,68 @@ describe('Testing AngularJS Zarad Profile Page', function(){
       });
 
       it('should return promise when editing profile', function(){
+        scope.data.username = 'Plmoha3492';
+        scope.user = { username : 'Plmoha492', beltColor : 'Yellow'};
 
-        // deferred.resolve({data: {username : 'Plmoha429' , password: "02134u32", valid:false}})
+        deferred.resolve({data: {username : 'Plmoha429' , password: "02134u32", valid:false , beltColor : 'Yellow'}})
 
-        // scope.confirm();
-        // scope.$apply();
 
+        scope.confirm();
+        scope.$apply();
+
+        expect(User.editProfile).toHaveBeenCalled();
+      });
+
+      it('should return error', function(){
+        scope.data.username = 'Plmoha3492';
+        scope.user = { username : 'Plmoha492', beltColor : 'Yellow'};
+
+        deferred.reject('this is an error');
+
+        scope.confirm();
+        scope.$apply();
+
+        expect(User.editProfile).toHaveBeenCalled();
+        expect(true).toBe(true);
+      });
+
+
+      it('should show Popup When calling show password function', inject(function($q) {
+        var popup = spyOn(ionicPopupMock, 'show').and.callThrough().and.returnValue(deferred.promise);
+
+        scope.showPassWord();
+        expect(popup).toHaveBeenCalled();
+        expect(true).toBe(true);
+      }));
+
+      it('should show Popup When calling show confirm function', inject(function($q) {
+
+        var popup = spyOn(ionicPopupMock, 'confirm').and.callThrough().and.returnValue(deferred.promise);
+        scope.showConfirm();
+        expect(popup).toHaveBeenCalled();
+        expect(true).toBe(true);
+      }));
+
+      it('should show Popup When calling show date function', inject(function($q) {
+        var popup = spyOn(ionicPopupMock, 'alert').and.callThrough().and.returnValue(deferred.promise);
+        scope.showDate();
+        expect(popup).toHaveBeenCalled();
+        expect(true).toBe(true);
+      }));
+
+      it('should show Action sheet when calling show action function', inject(function($q) {
+        var popup = spyOn(ionicActionMock, 'show').and.callThrough().and.returnValue(deferred.promise);
+        scope.showAction();
+        expect(popup).toHaveBeenCalled();
+        expect(true).toBe(true);
+      }));
+
+
+      it('should logout when running the logout function', function(){
+        scope.logout();
+        scope.$apply();
+        expect(Auth.signout).toHaveBeenCalled();
+        expect(true).toBe(true);
       })
 
     });
@@ -120,8 +183,8 @@ describe('JS Login Page', function(){
 
     describe('Auth Controller Promise Tests', function () {
 
-      var $scope, $q,deferred,$window,Auth,User;
-      beforeEach(inject(function(_$controller_, _$rootScope_, _$location_, _User_, _$window_, _$ionicPopup_ , _Auth_, _$timeout_, _$q_) {
+      var $scope, $q,deferred,$window,Auth,User, ionicPopupMock;
+      beforeEach(inject(function(_$controller_, _$rootScope_, _$location_, _User_, _$window_, $ionicPopup , _Auth_, _$timeout_, _$q_) {
 
         $q = _$q_;
         $scope = _$rootScope_.$new();
@@ -129,6 +192,7 @@ describe('JS Login Page', function(){
         deferred = _$q_.defer();
         Auth = _Auth_;
         User = _User_;
+        ionicPopupMock = $ionicPopup;
         // Use a Jasmine Spy to return the deferred promise
         spyOn(_Auth_, 'signin').and.returnValue(deferred.promise);
         $window = _$window_
@@ -213,7 +277,15 @@ describe('JS Login Page', function(){
 
           expect(User.getUser).toHaveBeenCalled();
           expect($scope.error).toEqual('This is an error');
-        })
+        });
+
+        it('should show Popup When calling show date function', inject(function($q) {
+          var popup = spyOn(ionicPopupMock, 'show').and.callThrough().and.returnValue(deferred.promise);
+          $scope.showPopup();
+          expect(popup).toHaveBeenCalled();
+          expect(true).toBe(true);
+        }));
+
     });
 
   })
